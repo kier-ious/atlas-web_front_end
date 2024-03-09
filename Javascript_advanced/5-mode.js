53c5e4033810fdf731b8105901ec4b0835804a43
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-03-09 11:29:37 -0600

    using codepen I was able to get task 5 to work correctly.

---
+++ Javascript_advanced/5-mode.js
@@ -0,0 +1,36 @@
+function changeMode(size, weight, transform, background, color) {
+  return function() {
+    document.body.style.fontSize = size + 'px';
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  }
+}
+
+function main() {
+  let spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  let dark = changeMode(12, 'bold', 'capitalized', 'black', 'white');
+  let scream = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+
+  let paragraph = document.createElement('p');
+  paragraph.innerHTML = 'Welcome Holberton!';
+  document.body.appendChild(paragraph);
+
+  let spookyButton = document.createElement('button');
+  spookyButton.innerHTML = 'Spooky';
+  spookyButton.addEventListener('click', spooky);
+  document.body.appendChild(spookyButton);
+
+  let darkModeButton = document.createElement('button');
+  darkModeButton.innerHTML = 'Dark Mode';
+  darkModeButton.addEventListener('click', dark);
+  document.body.appendChild(darkModeButton);
+
+  let screamModeButton = document.createElement('button');
+  screamModeButton.innerHTML = 'Scream Mode';
+  screamModeButton.addEventListener('click', scream);
+  document.body.appendChild(screamModeButton);
+}
+
+main();
