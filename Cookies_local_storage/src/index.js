46d62d9ed487bbce44b8de1b7f23168db5e18cbc
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-03-18 16:41:43 -0500

    created all tasks and completed task 0 and added functions to src file

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,19 +1,15 @@
 function setCookies() {
-  var firstname = document.getElementById('firstname').value;
-  var email = document.getElementById('email').value;
+  var firstnameValue = document.getElementById('firstname').value;
+  var emailValue = document.getElementById('email').value;
 
-  document.cookie = `firstname=${firstname}`;
-  document.cookie = `email=${email}`;
+  document.cookie = `firstname=${firstnameValue}`;
+  document.cookie = `email=${emailValue}`;
 }
 
 function showCookies() {
-  var cookies = document.cookie.split(';');
-
-  var p = document.createElement('p');
-  p.innerHTML = 'Cookies';
-  cookies.forEach(cookie => {
-    p.innerHTML += '<br>' + cookie.trim();
-  });
-
-  document.body.appendChild(p);
+  var cookiesParagraph = document.createElement('p');
+  var cookiesText = 'Cookies: ' + document.cookie;
+  cookiesParagraph.innerhitml = cookiesText;
+  document.body.appendChild(cookiesParagraph);
+    console.log("Cookies:", document.cookie);
 }
