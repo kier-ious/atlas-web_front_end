ca957b242515034bcfcfbe8e6337a52d9d903877
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-03-19 13:17:50 -0500

    completed task 3-4

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,32 +1,93 @@
-function setCookies() {
+// fixed the onlick attr to eventListeners
+document.addEventListener('DOMContentLoaded', function() {
+  var loginButton = document.getElementById('loginButton');
+
+  loginButton.addEventListener('click', function() {
+    setCookiesAndShowWelcomeMessage();
+  })
+})
+
+function setCookiesAndShowWelcomeMessage() {
   var firstnameValue = document.getElementById('firstname').value;
   var emailValue = document.getElementById('email').value;
 
-var currentDate = new Date();
-var expirationDate = new Date(currentDate.getTime() + (10 * 24 * 60 * 60 * 1000));
+  var currentDate = new Date();
+  var expirationDate = new Date(currentDate.getTime() + (10 * 24 * 60 * 60 * 1000));
 
-  document.cookie = `firstname=${firstnameValue}; expires=${expirationDate.toUTCString()}; path=/`;
-  document.cookie = `email=${emailValue}; expires=${expirationDate.toUTCString()}; path=/`;
+  Cookies.remove('firstname', { path: '/'});
+  Cookies.remove('email', { path: '/'});
+
+
+  Cookies.set(`firstname=${firstnameValue}`, { expires: expirationDate, path: '/'});
+  Cookies.set(`email=${emailValue}`, { expires: expirationDate, path: '/'});
+
+  showWelcomeMessageOrForm();
 }
 
 function showCookies() {
-  var email = getCookie('email');
-  var firstname = getCookie('firstname');
+  var email = Cookies.get('email');
+  var firstname = Cookies.get('firstname');
 
   var cookiesParagraph = document.createElement('p');
   cookiesParagraph.textContent = `Email: ${email} - Firstname: ${firstname}`;
   document.body.appendChild(cookiesParagraph);
 }
 
-function getCookie(name) {
-  const cookieName = `${name}=`;
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(';');
-  for (let i = 0; i < cookieArray.length; i++) {
-    const cookie = cookieArray[i].trim();
-    if (cookie.indexOf(cookieName) === 0) {
-      return cookie.substring(cookieName.length, cookie.length);
+// function getCookie(name) {
+//   const cookieName = `${name}=`;
+//   const decodedCookie = decodeURIComponent(document.cookie);
+//   const cookieArray = decodedCookie.split(';');
+//   for (let i = 0; i < cookieArray.length; i++) {
+//     const cookie = cookieArray[i].trim();
+//     if (cookie.indexOf(cookieName) === 0) {
+//       return cookie.substring(cookieName.length, cookie.length);
+//     }
+//   }
+//   return '';
+// }
+
+function deleteCookiesAndShowForm() {
+  Cookies.remove('firstname', { path: '/'});
+  Cookies.remove('email', { path: '/'});
+  showForm();
+}
+
+    function showForm() {
+      hideWelcomeMessage();
+      document.getElementById('loginForm').style.display = 'block';
     }
+
+    function hideForm() {
+      document.getElementById('loginForm').style.display = 'none';
+    }
+
+function showWelcomeMessageOrForm() {
+  var firstname = getCookie('firstname');
+  if (firstname) {
+    welcomeMessage = document.createElement('h1');
+    welcomeMessage.textContent = `Welcome ${firstname} `;
+    var logoutLink = document.createElement('a');
+    logoutLink.textContent = '(logout)';
+    logoutLink.style.fontStyle = 'italic';
+    logoutLink.style.fontWeight = 'normal';
+    logoutLink.style.marginLeft = '10px';
+    logoutLink.href = '#';
+    logoutLink.onclick = function() {
+      deleteCookiesAndShowForm();
+      hideWelcomeMessage();
+      return false;
+    };
+    welcomeMessage.appendChild(logoutLink);
+  } else {
+    showForm();
   }
-  return '';
+  document.body.innerHTML = '';
+  document.body.appendChild(welcomeMessage);
+}
+
+function hideWelcomeMessage() {
+  var welcomeMessage = document.querySelector('h1');
+  if (welcomeMessage) {
+    welcomeMessage.remove();
+  }
 }
